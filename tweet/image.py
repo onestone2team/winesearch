6b49540189231053377d8141c868fe06ce5e2c00
hyojine <touroz@naver.com>
2022-11-03 00:46:33 +0900

    feat : image.py 수정

--- tweet/image.py
+++ tweet/image.py
@@ -1,12 +1,12 @@
-from urllib.request import urlretrieve
-from urllib.parse import quote_plus    
+from urllib.request import urlretrieve 
 from bs4 import BeautifulSoup as BS    
 from selenium import webdriver
 
+
 wine_name = input("wine : ")
-wine_name = wine_name.replace('','+')
+wine_name = wine_name.replace(' ','+')
 i_url = f'https://www.google.com/search?q={wine_name}&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi4rtfAq4_7AhXRq1YBHaI3B7oQ_AUoAnoECAEQBA&biw=967&bih=554&dpr=1.25#imgrc=aWODM-gjNv81jM'
-driver = webdriver.Chrome('C://chromedriver.exe')
+driver = webdriver.Chrome('C:/Users/hyojine/Downloads/chromedriver_win32/chromedriver.exe')
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver.get(i_url)
@@ -17,7 +17,7 @@ soup = BS(html,features="html.parser")
 img = soup.select('img')
 
 image_list = []
-count = 1
+# count = 1
 
 print("Searching...")
 for i in img:
@@ -27,9 +27,10 @@ for i in img:
       image_list.append(i.attrs["data-src"])
 
 print("Downloading...")
-for i in image_list:
-   urlretrieve(i,"download/"+wine_name+str(count)+".jpg")
-   count+=1
+for image in image_list:
+   urlretrieve(image,"C:/Users/hyojine/Pictures/wine_search_image/"+str(wine_name)+".jpg")
+   # urlretrieve(image,"download/"+wine_name+str(count)+".jpg")
+   # count+=1
 
 driver.close()
 print("집에가자")
